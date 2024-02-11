ab32268e1677a90acd9fd53fdaed2a19c587062c
shayshunk <60236335+shayshunk@users.noreply.github.com>
2024-02-10 23:47:45 -0500

    Set up class.

--- BiPo.cc
+++ BiPo.cc
@@ -59,3 +59,78 @@ void FillDetectorConfig()
         cout << "--------------------------------------------\n";
     }
 }
+
+bool CheckNeighbor(int segment, char direction)
+{
+    // Used for dead segment calculations
+
+    bool neighbor = false;
+
+    switch (direction)
+    {
+        case 'r':
+            neighbor = detectorConfig[segment + 1];
+            break;
+        case 'l':
+            neighbor = detectorConfig[segment - 1];
+            break;
+        case 'u':
+            neighbor = detectorConfig[segment + 14];
+            break;
+        case 'd':
+            neighbor = detectorConfig[segment - 14];
+            break;
+        default:
+            cout << "That direction doesn't exist!\n";
+            return false;
+    }
+
+    return neighbor;
+}
+
+BiPo::BiPo()
+{
+    for (int dataset = Data; dataset < DatasetSize; dataset++)  // Dataset
+    {
+        for (int signalSet = Correlated; signalSet < TotalDifference; signalSet++)  // Signal
+        {
+            for (int direction = X; direction < DirectionSize; direction++)
+            {
+                string data = DatasetToString(dataset);
+                string signal = SignalToString(signalSet);
+                string axis = AxisToString(direction);
+                string histogramName = data + " " + signal + " " + axis;
+
+                int bins;
+                switch (direction)
+                {
+                    case X:
+                        bins = xBins;
+                        break;
+                    case Y:
+                        bins = xBins;
+                        break;
+                    case Z:
+                        bins = zBins;
+                        break;
+                    default:
+                        bins = xBins;
+                }
+
+                histogram[dataset][signalSet][direction]
+                    = TH1F(histogramName.c_str(), data.c_str(), bins, -histogramMax, histogramMax);
+            }
+        }
+    }
+
+    ResetLineNumber();
+}
+
+void BiPo::ReadFileList() {}
+void BiPo::SetUpHistograms() {}
+void BiPo::FillHistogramUnbiased(int signalSet) {}
+void BiPo::FillHistogram() {}
+void BiPo::CalculateUnbiasing() {}
+void BiPo::SubtractBackgrounds() {}
+void BiPo::CalculateCovariances() {}
+void BiPo::FillOutputFile() {}
--- BiPo.h
+++ BiPo.h
@@ -3,6 +3,8 @@
 #include <iostream>
 #include <string>
 
+#include "TH1F.h"
+
 // Invariables
 
 // Variables
@@ -71,3 +73,89 @@ std::string SignalToString(int num)
 
     return name;
 }
+
+enum Datasets
+{
+    Data = 0,
+    DataUnbiased,
+    DatasetSize
+};
+
+std::string DatasetToString(int num)
+{
+    std::string name;
+
+    switch (num)
+    {
+        case 0:
+            name = "Data";
+            break;
+        case 1:
+            name = "Data Unbiased";
+            break;
+        default:
+            name = "Data";
+    }
+
+    return name;
+}
+
+class BiPo
+{
+  public:
+    // Variables
+    double livetimeOff = 0, livetimeOn = 0;
+
+    // Main functions
+    BiPo();
+    void ReadFileList();
+    void SetUpHistograms();
+    void FillHistogramUnbiased(int signalSet);
+    void FillHistogram();
+    void CalculateUnbiasing();
+    void SubtractBackgrounds();
+    void CalculateCovariances();
+    void FillOutputFile();
+
+    // Inline functions
+    inline void ResetLineNumber() { lineNumber = 0; }
+    inline void ResetLineCounter() { lineCounter = 0; }
+    inline void ResetIndex() { index = 0; }
+
+  private:
+    // Histogram to count IBDs
+    std::array<std::array<std::array<TH1F, DirectionSize>, SignalSize>, DatasetSize> histogram;
+
+    // File list
+    std::array<std::string, 4045> files;
+
+    // Values grabbed from ROOT tree
+    double Esmear;
+    double nCaptTime;
+    double xRx;
+    double promptPosition, delayedPosition;
+    int promptSegment, delayedSegment;
+    int dataSet;
+    int direction;
+    int lineNumber = 0, lineCounter = 0;
+    std::size_t index = 0;
+
+    // Invariables
+    static constexpr int xBins = 301;  // Number of bins for our histograms
+    static constexpr int zBins = 801;
+    static constexpr int totalDataLines = 4040;
+    static constexpr int totalSimLines = 500;
+    static constexpr float histogramMax = 150.5;  // Maximum value of bin for histograms
+    static constexpr float segmentWidth = 145.7;  // Distance between segment centers in mm
+    static constexpr float atmosphericScaling = 1.000254;  // Atmosphering scaling coefficient
+    static constexpr char* dataPath = "2019XList_RxOff.txt";  // Reactor off dataset
+    static constexpr char* dataFileName
+        = "/home/shay/Documents/PROSPECTData/BiPo_Data/%s/AD1_BiPo.root";
+
+    // Storing final counts
+    std::array<std::array<float, DirectionSize>, DatasetSize> effectiveIBD;
+    std::array<std::array<float, DirectionSize>, DatasetSize> totalIBD;
+    std::array<std::array<float, DirectionSize>, DatasetSize> totalIBDError;
+    std::array<std::array<float, DirectionSize>, DatasetSize> mean;
+    std::array<std::array<float, DirectionSize>, DatasetSize> sigma;
+};
